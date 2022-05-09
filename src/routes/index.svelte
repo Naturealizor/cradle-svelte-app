aa676713d4d8953818a62ab140fffeb984f45ec9
Jacob Jordan <31334963+Naturealizor@users.noreply.github.com>
2022-05-09 12:55:12 -0400

    set up dev environment and started front page

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<div />
+
+<header>
+	<img src="images/stationary logo 3.jpg" alt="cradle logo" />
+	<img src="images/Group_63.png" alt="cradle logo" />
+
+	<ul>
+		<li><a href="">About Us</a></li>
+		<li><a href="">Gallery</a></li>
+		<li><a href="">Contact Us</a></li>
+	</ul>
+</header>
+<main>
+	<img src="images/Cavernet-Sauvignon-Grapes-on-the-vine 1.jpg" alt="grape vine" />
+</main>
+
+<style>
+	main {
+		width: 100%;
+	}
+</style>
